1dcd6618e5c7d4f4ff75d3c0e9b210d05b506bb1
cgWoofus <charlesbryanguerrero@yahoo.com>
2018-03-13 01:44:36 +0800

    __

---
+++ Patrol.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace Assets.scripts._Attributes
+{
+    public delegate void UpdateTransform(Transform _target);
+    [ExecuteInEditMode]
+    public class Patrol: Trait
+    {
+        [SerializeField]
+        protected Vector3[] _targetPositions;
+        public float _speed;
+        Vector3 _currentPosition;
+
+        [SerializeField]
+        GameObject[] _targetIndicators;
+        protected override bool HasTarget()
+        {
+            return true;
+        }
+
+        public void init(float speed,int numberOfIndicators)
+        {
+            _speed = speed;
+            upDateTransform = upDateCurrentPosition;
+
+        }
+
+        void upDateCurrentPosition(Transform  _target)
+        {
+
+        }
+
+        private void Update()
+        {
+            for(int x=0; x< _targetIndicators.Length;x++)
+            {
+                if (_targetPositions.Length != _targetIndicators.Length)
+                    SetPositions(_targetIndicators.Length);
+                _targetPositions[x] = _targetIndicators[x].transform.position;
+            }
+        }
+
+        void SetPositions(int length,params GameObject[] positions)
+        {
+            _targetPositions = new Vector3[length];
+
+        }
+
+        protected override MeshFilter GetMesh(int index)
+        {
+            return _meshCollection[index];
+        }
+        
+    }
+}
---
+++ Trait.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Assets.scripts._Attributes
+{
+public abstract class Trait : MonoBehaviour //,moods
+{
+    [SerializeField]
+    protected MeshFilter[] _meshCollection;
+    protected UpdateTransform upDateTransform;
+    protected abstract bool HasTarget();
+    protected abstract  MeshFilter GetMesh(int index);
+
+
+
+}
+}
